a34e87272e6b8c806df51dc656457f73b07777a4
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2023-02-10 01:22:23 +0000

    Fixed fish data, branch ready to be merged when front end is done

--- src/index.ts
+++ src/index.ts
@@ -17,194 +17,196 @@ export interface IPullCatchReqBody {
   Season: string;
   Date: string;
 }
-async function main(): Promise<void> {
-  try {
-    const app: Application = express();
-    app.use(compression());
-    app.use(cors());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
-    const client: Db = await MongoConnector.connectToMongo();
-    app.post("/register", async (req: Request, resp: Response) => {
-      if (!req.body.email || !req.body.password) {
-        resp.status(422);
-        resp.send("Email or password not given");
-      } else {
-        const email: string = req.body.email;
-        const pass: string = req.body.password;
-        try {
-          await new SubmitUser(email, pass, client).submitUser();
-          resp.status(200);
-          resp.send("Account created");
-        } catch (e) {
-          let message = "Internal server error";
-          if (e instanceof Error) message = e.message;
-          resp.status(500);
-          resp.send(message);
-        }
-      }
-    });
-    app.post("/login", async (req: Request, resp: Response) => {
-      if (!req.body.email || !req.body.password) {
-        resp.status(422);
-        resp.send("Email or password not given");
-      } else {
-        const email: string = req.body.email;
-        const pass: string = req.body.password;
-        try {
-          const token: ITokenAccountObj | null = await new LoginUser(
-            email,
-            pass,
-            client
-          ).login();
-          if (token) {
-            resp.status(200);
-            resp.send(token);
-          } else {
-            resp.status(500);
-            resp.send("Password inncorect");
-          }
-        } catch (e) {
-          let message = "Internal server error";
-          if (e instanceof Error) message = e.message;
-          resp.status(500);
-          resp.send(message);
-        }
-      }
-    });
-    app.post("/deleteaccount", async (req: Request, resp: Response) => {
-      if (!req.headers.authorization || !req.body.password) {
-        resp.status(422);
-        resp.send("No token/password included");
-      } else {
-        const token: string = req.headers.authorization;
-        const password: string = req.body.password;
-        try {
-          const tokenDetails = Token.decodeToken(token);
+abstract class Program {
+  public static async main(): Promise<void> {
+    try {
+      const app: Application = express();
+      app.use(compression());
+      app.use(cors());
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      const client: Db = await MongoConnector.connectToMongo();
+      app.post("/register", async (req: Request, resp: Response) => {
+        if (!req.body.email || !req.body.password) {
+          resp.status(422);
+          resp.send("Email or password not given");
+        } else {
+          const email: string = req.body.email;
+          const pass: string = req.body.password;
           try {
-            await new LoginUser(
-              tokenDetails.user,
-              password,
+            await new SubmitUser(email, pass, client).submitUser();
+            resp.status(200);
+            resp.send("Account created");
+          } catch (e) {
+            let message = "Internal server error";
+            if (e instanceof Error) message = e.message;
+            resp.status(500);
+            resp.send(message);
+          }
+        }
+      });
+      app.post("/login", async (req: Request, resp: Response) => {
+        if (!req.body.email || !req.body.password) {
+          resp.status(422);
+          resp.send("Email or password not given");
+        } else {
+          const email: string = req.body.email;
+          const pass: string = req.body.password;
+          try {
+            const token: ITokenAccountObj | null = await new LoginUser(
+              email,
+              pass,
               client
-            ).deleteUser();
-            resp.status(200);
-            resp.send("Account deleted");
+            ).login();
+            if (token) {
+              resp.status(200);
+              resp.send(token);
+            } else {
+              resp.status(500);
+              resp.send("Password inncorect");
+            }
           } catch (e) {
             let message = "Internal server error";
             if (e instanceof Error) message = e.message;
             resp.status(500);
             resp.send(message);
           }
-        } catch (e) {
-          resp.status(498);
-          resp.send("Bad token given");
         }
-      }
-    });
-    app.post("/pullcatches", async (req: Request, resp: Response) => {
-      if (!req.headers.authorization) {
-        resp.status(498);
-        resp.send("No token included");
-      } else {
-        const token: string = req.headers.authorization;
-        try {
-          const tokenDetails = Token.decodeToken(token);
+      });
+      app.post("/deleteaccount", async (req: Request, resp: Response) => {
+        if (!req.headers.authorization || !req.body.password) {
+          resp.status(422);
+          resp.send("No token/password included");
+        } else {
+          const token: string = req.headers.authorization;
+          const password: string = req.body.password;
           try {
-            const myCatches = await new FishLoadOperations(
-              client,
-              tokenDetails.user
-            ).getCatches(
-              Object.keys(req.body).length !== 0 &&
-                typeof req.body === "object" &&
-                req.body
-            );
-            resp.status(200);
-            resp.send(myCatches);
+            const tokenDetails = Token.decodeToken(token);
+            try {
+              await new LoginUser(
+                tokenDetails.user,
+                password,
+                client
+              ).deleteUser();
+              resp.status(200);
+              resp.send("Account deleted");
+            } catch (e) {
+              let message = "Internal server error";
+              if (e instanceof Error) message = e.message;
+              resp.status(500);
+              resp.send(message);
+            }
           } catch (e) {
-            let message = "Internal server error";
-            if (e instanceof Error) message = e.message;
-            resp.status(500);
-            resp.send(message);
+            resp.status(498);
+            resp.send("Bad token given");
           }
-        } catch (e) {
-          resp.status(498);
-          resp.send("Bad token given");
         }
-      }
-    });
-    app.post("/postcatch", async (req: Request, resp: Response) => {
-      if (!req.headers.authorization) {
-        resp.status(498);
-        resp.send("No token included");
-      } else {
-        const token = req.headers.authorization;
-        try {
-          const tokenDetails = Token.decodeToken(token);
+      });
+      app.post("/pullcatches", async (req: Request, resp: Response) => {
+        if (!req.headers.authorization) {
+          resp.status(498);
+          resp.send("No token included");
+        } else {
+          const token: string = req.headers.authorization;
           try {
-            await new FishLogOperations(
-              tokenDetails.user,
-              req.body,
-              client
-            ).submitCatch();
-            resp.status(200);
-            resp.send("Catch submitted.");
+            const tokenDetails = Token.decodeToken(token);
+            try {
+              const myCatches = await new FishLoadOperations(
+                client,
+                tokenDetails.user
+              ).getCatches(
+                Object.keys(req.body).length !== 0 &&
+                  typeof req.body === "object" &&
+                  req.body
+              );
+              resp.status(200);
+              resp.send(myCatches);
+            } catch (e) {
+              let message = "Internal server error";
+              if (e instanceof Error) message = e.message;
+              resp.status(500);
+              resp.send(message);
+            }
           } catch (e) {
-            let message = "Internal server error";
-            if (e instanceof Error) message = e.message;
-            resp.status(500);
-            resp.send(message);
+            resp.status(498);
+            resp.send("Bad token given");
           }
-        } catch (e) {
-          resp.status(498);
-          resp.send("Bad token given");
         }
-      }
-    });
-    app.post("/deletecatches", async (req: Request, resp: Response) => {
-      if (!req.headers.authorization) {
-        resp.status(498);
-        resp.send("No token included");
-      } else {
-        const token: string = req.headers.authorization;
-        try {
-          const tokenDetails = Token.decodeToken(token);
+      });
+      app.post("/postcatch", async (req: Request, resp: Response) => {
+        if (!req.headers.authorization) {
+          resp.status(498);
+          resp.send("No token included");
+        } else {
+          const token = req.headers.authorization;
           try {
-            if (!Array.isArray(req.body))
-              throw new Error("Please provide valid Array to delete");
-            await Promise.all(
-              req.body.map((ele) => {
-                if (!("properties" in ele) || !("geometry" in ele))
-                  throw new Error(
-                    "Please provide valid GeoJson catches to delete"
-                  );
-                const newEle = ele.properties;
-                newEle.Latitude = ele.geometry.coordinates[0];
-                newEle.Longitude = ele.geometry.coordinates[1];
-                return new FishLogOperations(
-                  tokenDetails.user,
-                  newEle,
-                  client
-                ).deleteCatch();
-              })
-            );
-            resp.status(200);
-            resp.send("Catches deleted");
+            const tokenDetails = Token.decodeToken(token);
+            try {
+              await new FishLogOperations(
+                tokenDetails.user,
+                req.body,
+                client
+              ).submitCatch();
+              resp.status(200);
+              resp.send("Catch submitted.");
+            } catch (e) {
+              let message = "Internal server error";
+              if (e instanceof Error) message = e.message;
+              resp.status(500);
+              resp.send(message);
+            }
           } catch (e) {
-            let message = "Internal server error";
-            if (e instanceof Error) message = e.message;
-            resp.status(500);
-            resp.send(message);
+            resp.status(498);
+            resp.send("Bad token given");
           }
-        } catch (e) {
-          resp.status(498);
-          resp.send("Bad token given");
         }
-      }
-    });
-    const portVar: number = Number(process.env.PORT) || 5000;
-    app.listen(portVar, "0.0.0.0", () =>
-      console.log(`\n\nServer running on port: ${portVar}\n\n`)
-    );
-  } catch (e) {}
+      });
+      app.post("/deletecatches", async (req: Request, resp: Response) => {
+        if (!req.headers.authorization) {
+          resp.status(498);
+          resp.send("No token included");
+        } else {
+          const token: string = req.headers.authorization;
+          try {
+            const tokenDetails = Token.decodeToken(token);
+            try {
+              if (!Array.isArray(req.body))
+                throw new Error("Please provide valid Array to delete");
+              await Promise.all(
+                req.body.map((ele) => {
+                  if (!("properties" in ele) || !("geometry" in ele))
+                    throw new Error(
+                      "Please provide valid GeoJson catches to delete"
+                    );
+                  const newEle = ele.properties;
+                  newEle.Latitude = ele.geometry.coordinates[0];
+                  newEle.Longitude = ele.geometry.coordinates[1];
+                  return new FishLogOperations(
+                    tokenDetails.user,
+                    newEle,
+                    client
+                  ).deleteCatch();
+                })
+              );
+              resp.status(200);
+              resp.send("Catches deleted");
+            } catch (e) {
+              let message = "Internal server error";
+              if (e instanceof Error) message = e.message;
+              resp.status(500);
+              resp.send(message);
+            }
+          } catch (e) {
+            resp.status(498);
+            resp.send("Bad token given");
+          }
+        }
+      });
+      const portVar: number = Number(process.env.PORT) || 5000;
+      app.listen(portVar, "0.0.0.0", () =>
+        console.log(`\n\nServer running on port: ${portVar}\n\n`)
+      );
+    } catch (e) {}
+  }
 }
-main();
+Program.main();
